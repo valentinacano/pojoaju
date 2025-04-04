be2313e0d0fc72f57e794b38566dd288ee2827af
valentinacano <valentinacanol99@gmail.com>
2025-04-03 21:13:13 -0400

    ML: Agregar funcion capture_sample

---
+++ ml/features/capture_samples.py
@@ -0,0 +1,76 @@
+import os
+import cv2
+import numpy as np
+from mediapipe.python.solutions.holistic import Holistic
+from app.utils.capture_samples import save_frames, draw_keypoints
+from app.utils.general import create_folder, mediapipe_detection, there_hand
+from app.config import FONT, FONT_POS, FONT_SIZE
+from datetime import datetime
+
+
+def capture_samples(path, margin_frame=1, min_cant_frames=5, delay_frames=3):
+    """
+    ### CAPTURA DE MUESTRAS PARA UNA PALABRA
+    Recibe como parámetro la ubicación de guardado y guarda los frames
+
+    `path` ruta de la carpeta de la palabra \n
+    `margin_frame` cantidad de frames que se ignoran al comienzo y al final \n
+    `min_cant_frames` cantidad de frames minimos para cada muestra \n
+    `delay_frames` cantidad de frames que espera antes de detener la captura después de no detectar manos
+    """
+    create_folder(path)
+
+    count_frame = 0
+    frames = []
+    fix_frames = 0
+    recording = False
+
+    with Holistic() as holistic_model:
+        video = cv2.VideoCapture(0)
+
+        while video.isOpened():
+            ret, frame = video.read()
+            if not ret:
+                break
+
+            image = frame.copy()
+            results = mediapipe_detection(frame, holistic_model)
+
+            if there_hand(results) or recording:
+                recording = False
+                count_frame += 1
+                if count_frame > margin_frame:
+                    cv2.putText(
+                        image, "Capturando...", FONT_POS, FONT, FONT_SIZE, (255, 50, 0)
+                    )
+                    frames.append(np.asarray(frame))
+            else:
+                if len(frames) >= min_cant_frames + margin_frame:
+                    fix_frames += 1
+                    if fix_frames < delay_frames:
+                        recording = True
+                        continue
+                    frames = frames[: -(margin_frame + delay_frames)]
+                    today = datetime.now().strftime("%y%m%d%H%M%S%f")
+                    output_folder = os.path.join(path, f"sample_{today}")
+                    create_folder(output_folder)
+                    save_frames(frames, output_folder)
+
+                recording, fix_frames = False, 0
+                frames, count_frame = [], 0
+                cv2.putText(
+                    image,
+                    "Listo para capturar...",
+                    FONT_POS,
+                    FONT,
+                    FONT_SIZE,
+                    (0, 220, 100),
+                )
+
+            draw_keypoints(image, results)
+            cv2.imshow(f'Toma de muestras para "{os.path.basename(path)}"', image)
+            if cv2.waitKey(10) & 0xFF == ord("q"):
+                break
+
+        video.release()
+        cv2.destroyAllWindows()
